e103706ae948a4aa490277cf55757f787c061ada
Novikov Aleksei <3900676@gmail.com>
2022-09-01 22:18:27 +0300

    added remote serv

--- src/test/java/com/demoqa/tests/TestBaseConfig.java
+++ src/test/java/com/demoqa/tests/TestBaseConfig.java
@@ -1,12 +1,17 @@
 package com.demoqa.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
 
 public class TestBaseConfig {
     @BeforeAll
     static void configure() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1200x800";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 }
